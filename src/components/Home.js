bc1b7db873a7621b65cb2016dca129d885d724bd
flybonzai <flybonzai@gmail.com>
2021-01-08 20:25:06 -0700

    Further tinkering with spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import '../stylesheets/Home.css'
-import Header from './Header'
-import Footer from './Footer'
-import photoImg from '../assets/images/artist_photo.jpg'
+import React from "react";
+import "../stylesheets/Home.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import photoImg from "../assets/images/artist_photo.jpg";
 
+// TODO split out additional components
+// make all class names lowercase with hyphens
+// Add semantic elements
 const Home = (props) => {
   return (
-    <div className={'Content'} id={'Home'}>
-      <Header/>
-      <div className="ArtistPhotoAndDesc">
-        <span className="ArtistShortDesc">
+    <div className={"Content"} id={"Home"}>
+      <Header />
+      <section className="ArtistPhotoAndDesc">
+        <div className="ArtistShortDesc">
           A shirtful of sand, and organs created from stone...
-          <br/>
+          <br />
+          <br />
           Here's to finding a way back home
-          <br/>
-          <br/>
+          <br />
+          <br />
           - <em>Bones</em>
-        </span>
-        <img src={photoImg} alt="Artist photo"/>
-      </div>
+        </div>
+        <img src={photoImg} alt="Artist photo" />
+      </section>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

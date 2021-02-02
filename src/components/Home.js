7a54275c91993d0946bfb1dad211a5ee33ccf499
flybonzai <flybonzai@gmail.com>
2021-02-02 13:53:37 -0700

    Changes, ch-ch-ch changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,27 +9,37 @@ const Home = (props) => {
       <div>
         <Header />
         <section className="home-page-photo">
-          {/*TODO add a little caption of some sort to the left*/}
-          <div className={"short-description"}>
+          <div className={"home-page-content"}>
             <p className={"short-description-text"}>
-              "Their music sounds like the heavens parted and God sent down his angels in melodious form.
+              "Their music sounds like the heavens parted and God sent down his
+              angels in melodious form.
               <br />
-              It has a soul-funk-swing vibe to it that is hard to stop listining to."
+              It has a soul-funk-swing vibe to it that is hard to stop listening
+              to."
               <br />
+              <br />
+              <span className={"short-description-attribution"}>
+                - The Cincinnati Music Critic
+              </span>
             </p>
-            <span className={"short-description-attribution"}>
-            - The Cincinnati Music Critic
-            </span>
+            <section className="subscribe-form">
+              <form>
+                <label for="email">Subscribe for updates</label>
+                <br />
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  formMethod="post"
+                />
+                <button type="submit">Submit</button>
+              </form>
+            </section>
           </div>
-          <img src={"https://source.unsplash.com/random/600x400"} alt="Artist photo" />
-        </section>
-        <section className="subscribe-form">
-          <form>
-            <label for="email">Subscribe for updates</label>
-            <br />
-            <input type="email" id="email" placeholder="Email" formMethod="post"/>
-            <button type="submit">Submit</button>
-          </form>
+          <img
+            src={"https://source.unsplash.com/random/600x400"}
+            alt="Artist photo"
+          />
         </section>
       </div>
       <Footer />
